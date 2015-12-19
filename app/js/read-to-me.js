c319a4271ffbc5fc49f9d11e80ce9b1dd3855efb
Hopeful Llama <jonlaw10170@gmail.com>
2015-12-19 00:09:07 +0000

    Changed to Angular Material

--- app/js/read-to-me.js
+++ app/js/read-to-me.js
@@ -1,36 +1,40 @@
-var readToMeApp = angular.module('ReadToMeApp', []);
+var readToMeApp = angular.module('ReadToMeApp', ['ngMaterial']);
 
 readToMeApp.controller('ReadToMeController', ['$scope', function($scope) {
-	var speak = false;
+	$scope.speak = false;
 	$scope.sentenceIndex = 0;
 	$scope.toggleSpeak = function() {
-		speak = !speak;
-
-		$scope.parsedScript = $scope.script.trim().split(".");
-		if($scope.parsedScript[$scope.parsedScript.length-1] === "") {
-			$scope.parsedScript.splice($scope.parsedScript.length-1, 1);
+		if(typeof $scope.script !== 'undefined' && $scope.script !== null) {
+			$scope.speak = !$scope.speak;
+			$scope.parsedScript = $scope.script.trim().split(".");
+			if($scope.parsedScript[$scope.parsedScript.length-1] === "") {
+				$scope.parsedScript.splice($scope.parsedScript.length-1, 1);
+			}	
+			sayNextSentence();
+		} else {
+			$scope.speak = false;
 		}
-
-		var sayNextSentence = function() {
-			if($scope.parsedScript.length === $scope.sentenceIndex) {
-				$scope.sentenceIndex = 0;
-				speak = false;
-			}
-
-			if(speak) {
-				responsiveVoice.speak($scope.parsedScript[$scope.sentenceIndex], "UK English Female", {onend: function() {
-					$scope.sentenceIndex++;
-					sayNextSentence();
-				}});
-			}
-		};
-		sayNextSentence();
 	};
 
-	$scope.processFile = function() {
+	var sayNextSentence = function() {
+		if($scope.parsedScript.length === $scope.sentenceIndex) {
+			$scope.sentenceIndex = 0;
+			$scope.speak = false;
+			$scope.$digest();
+		}
+
+		if($scope.speak) {
+			responsiveVoice.speak($scope.parsedScript[$scope.sentenceIndex], "UK English Female", {onend: function() {
+				$scope.sentenceIndex++;
+				sayNextSentence();
+			}});
+		}
+	};
+
+	$scope.processFile = function(domId) {
 		var canvas = document.createElement("canvas");
 		var context = canvas.getContext('2d');
-		var f = document.getElementById('fileUpload').files[0];
+		var f = document.getElementById(domId).files[0];
 		var image = new Image;
 		image.onload = function() {
 			canvas.width = this.width
